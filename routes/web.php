0053b3fbfa8df8d05182944c45f1605ad2f359f6
ZildjianVito <zildjianvito07@gmail.com>
2024-03-21 20:44:28 +0700

    fix: redirect from home to notes page

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-// Route::redirect('/', '/notes', 301);
+Route::redirect('/', '/notes', 301);
 
-Route::get("/", function () {
-    return view("home");
-});
+
 
 Route::resource("/notes", NoteController::class);
